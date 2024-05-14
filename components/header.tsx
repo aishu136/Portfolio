23cd1900a3a94a62f9534917e88f567bb02c0bf6
aishwarya <46559921+aishu136@users.noreply.github.com>
2024-05-14 11:23:23 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,31 @@
 "use client";
 
-import React from "react";
+import React, { useState , useEffect  } from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 import clsx from "clsx";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 export default function Header() {
   const { activeSection, setActiveSection, setTimeOfLastClick } =
     useActiveSectionContext();
+   const [nav, setNav] = useState(false);
+     const handleNav = () => {
+    setNav(!nav);
+  };
+   const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+
+    return () => window.removeEventListener('resize', checkScreenSize);
+  }, []);
+
 
   return (
     <header className="z-[999] relative">
@@ -20,8 +36,14 @@ export default function Header() {
       ></motion.div>
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
-          {links.map((link) => (
+          <button onClick={handleNav} className={`text-3xl z-20 ${!isMobile && 'hidden'} ml-60`}>
+          {nav ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </button>
+          {!isMobile &&(
+      <ul className= "flex flex-row items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 relative">
+          
+             {links.map((link) => (
+         
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
               key={link.hash}
@@ -59,6 +81,50 @@ export default function Header() {
             </motion.li>
           ))}
         </ul>
+        )}
+        {isMobile && nav && (
+        <ul className="flex flex-col items-center justify-center gap-x-1 text-[0.4rem] font-medium text-gray-500 relative">
+          {links.map((link) => (
+            <motion.li
+              className="h-1/4  flex items-center justify-center relative"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
+              <Link
+                className={clsx(
+                  "flex w-full items-center justify-center  hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  {
+                    "text-gray-950 dark:text-gray-200":
+                      activeSection === link.name,
+                  }
+                )}
+                href={link.hash}
+                onClick={() => {
+                  setActiveSection(link.name);
+                  setTimeOfLastClick(Date.now());
+                }}
+              >
+                {link.name}
+    
+                {link.name === activeSection && (
+                  <motion.span
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
+                )}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+
+        )}
+        
       </nav>
     </header>
   );
